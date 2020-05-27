8c1a8ba7f561ba45ac84cbb1aa0ee230f15a65f1
James Phillips <jamesp2.phillips@gmail.com>
2020-05-27 15:55:12 +0100

    Previous Work gallery comes from JSON now

--- src/pages/previouswork.js
+++ src/pages/previouswork.js
@@ -1,118 +1,48 @@
-import React, { useCallback, useState } from "react"
+import React from "react"
 
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import FilterGallery from "../components/filterGallery"
 
-const photos = [
-  {
-    id: 1,
-    title: "Hello Cat",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/1280/1280",
-    width: 4,
-    height: 4,
-    category: "corporate",
-  },
-  {
-    id: 2,
-    title: "Hello Cat 2 ",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/1000/1000",
-    width: 4,
-    height: 4,
-    category: "live",
-  },
-  {
-    id: 3,
-    title: "Hello Cat 3",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/800/800",
-    width: 4,
-    height: 4,
-    category: "live",
-  },
-  {
-    id: 4,
-    title: "Hello Cat",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/900/900",
-    width: 4,
-    height: 4,
-    category: "corporate",
-  },
-  {
-    id: 5,
-    title: "Hello Cat",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/g/1280/1280",
-    width: 4,
-    height: 4,
-    category: "corporate",
-  },
-  {
-    id: 6,
-    title: "Hello Cat",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/g/1000/1000",
-    width: 4,
-    height: 4,
-    category: "live",
-  },
-  {
-    id: 7,
-    title: "Hello Cat",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/g/800/800",
-    width: 4,
-    height: 4,
-    category: "live",
-  },
-  {
-    id: 8,
-    title: "Hello Cat",
-    description: "Lorem Ipsum I am a Description",
-    credits: "PlaceKitten",
-    src: "http://placekitten.com/500/500",
-    width: 4,
-    height: 4,
-    category: "corporate",
-  },
-]
+const PreviousWorkPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Previous Work" />
 
-const categories = [
-  {
-    id: 0,
-    name: "all",
-    title: "All",
-  },
-  {
-    id: 1,
-    name: "live",
-    title: "Live",
-  },
-  {
-    id: 2,
-    name: "corporate",
-    title: "Corporate Event",
-  },
-]
+      <h2 className="thin">Previous Work</h2>
 
-const PreviousWorkPage = () => (
-  <Layout>
-    <SEO title="Previous Work" />
+      <FilterGallery
+        photos={data.dataJson.previousWorkGallery.photos}
+        categories={data.dataJson.previousWorkGallery.categories}
+      />
+    </Layout>
+  )
+}
 
-    <h2 className="thin">Previous Work</h2>
-
-    <FilterGallery photos={photos} categories={categories} />
-  </Layout>
-)
+export const query = graphql`
+  {
+    dataJson {
+      previousWorkGallery {
+        categories {
+          id
+          name
+          title
+        }
+        photos {
+          category
+          description
+          credits
+          height
+          id
+          src
+          title
+          width
+        }
+      }
+    }
+  }
+`
 
 export default PreviousWorkPage
