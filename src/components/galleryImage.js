1e4eb982dedc51667b1a1fc568416898a772b9eb
James Phillips <jamesp2.phillips@gmail.com>
2020-05-27 20:45:02 +0100

    transition images on hover

--- src/components/galleryImage.js
+++ src/components/galleryImage.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 const GalleryImage = ({
   containerHeight,
@@ -10,19 +10,34 @@ const GalleryImage = ({
   photo,
   top,
 }) => {
-  const imageStyle = {
-    margin: margin,
+  const [mouseEntered, setMouseEntered] = useState(false)
+
+  let imageStyle = {
     display: "block",
     cursor: "pointer",
+    transition: "transform 0.5s, filter 0.25s",
   }
 
   const handleClick = event => {
     onClick(event, { photo, index })
   }
 
+  if (mouseEntered)
+    imageStyle = {
+      ...imageStyle,
+      transform: "scale(1.1) rotate(-5deg)",
+      filter: "saturate(1.3)",
+    }
+
   return (
     <>
-      <div role="presentation" onClick={handleClick}>
+      <div
+        role="presentation"
+        onClick={handleClick}
+        onMouseEnter={() => setMouseEntered(true)}
+        onMouseLeave={() => setMouseEntered(false)}
+        style={{ overflow: "hidden", margin: margin }}
+      >
         <img alt={photo.title} {...photo} style={{ ...imageStyle }} />
       </div>
     </>
